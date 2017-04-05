95de8a9804137ae6375d43707daaaac1f58e4740
Oleg Slobodskoi <oleg008@gmail.com>
2017-04-05 02:19:09 +0200

    support jss 7

--- src/index.js
+++ src/index.js
@@ -5,19 +5,27 @@ export const marker = '__jss__'
 export default () => {
   const cache = Object.create(null)
 
-  function onCreateRule(name, decl) {
-    if (decl[marker]) return cache[decl[marker]]
+  function onCreateRule(name, decl, {parent}) {
+    if (parent) {
+      const selector = parent.rules.raw[name][marker]
+      if (selector) return cache[selector]
+    }
+
     return null
   }
 
   function onProcessRule(rule) {
-    const {selector, type, options: {sheet}} = rule
+    const {selector, type, options: {sheet, parent}} = rule
 
     if (type !== 'regular' || cache[selector]) return
+    if (!parent) return
     if (sheet && sheet.options.link) return
 
     cache[selector] = rule
-    Object.defineProperty(rule.originalStyle, marker, {value: selector})
+    const originalStyle = parent.rules.raw[rule.name]
+    if (!originalStyle[marker]) {
+      Object.defineProperty(originalStyle, marker, {value: selector})
+    }
   }
 
   return {onCreateRule, onProcessRule}
